dca06c2362b0fed8ea5cbb4cfc3c00eb9754557e
Alexandre Ait-Ettajer <alexandre.aitettajer@gmail.com>
2020-11-25 11:30:39 -0500

    Finished First Implementation

--- dofs.cpp
+++ dofs.cpp
@@ -9,6 +9,177 @@
 using namespace	std;
 using namespace	Omega_h;
 
+// Finding the centroid of each mesh using parallel for loops
+Read<Real> findCentroid(Mesh& mesh, const int& dim) {
+  LO numElems = mesh.nelems();
+  LO numEdges = mesh.nedges();
+  LO numFaces;
+  if (numEdges != numElems) {
+    numFaces = mesh.nfaces();
+  }
+  LO numRegis;
+  if (numFaces != numElems) {
+    numRegis = mesh.nregions();
+  }
+  auto elems2verts = mesh.ask_elem_verts();
+  auto coords = mesh.coords();
+  Write<Real> centroids_w(dim*numElems,"Element Centroids");  
+  
+  auto lamb = OMEGA_H_LAMBDA(LO i) {
+    // 2D Case
+    if (dim == 2) {
+      // 2D-Triangles
+      if (numFaces != 0) {
+        auto vertIndices = gather_verts<3>(elems2verts,i);
+        auto vertCoords = gather_vectors<3,2>(coords, vertIndices);
+        for (int j = 0; j < 3; j++) {
+          // printf("Triangle %d, Vertex %d: %f %f\n", i, j,vertCoords[j][0], vertCoords[j][1]);
+          centroids_w[dim*i+0] += vertCoords[j][0]/3;
+          centroids_w[dim*i+1] += vertCoords[j][1]/3;
+        }
+      }
+      // 2D-Edges
+      else if (numEdges != 0) {
+        auto vertIndices = gather_verts<2>(elems2verts,i);
+        auto vertCoords = gather_vectors<2,2>(coords, vertIndices);
+        for (int j = 0; j < 2; j++) {
+          centroids_w[dim*i+0] += vertCoords[j][0]/2;
+          centroids_w[dim*i+1] += vertCoords[j][1]/2;
+        } 
+      }
+    }
+    // 3D Case
+    else if (dim == 3){
+      // 3D-Tets
+      if (numRegis != 0) {
+        auto vertIndices = gather_verts<4>(elems2verts,i);
+        auto vertCoords = gather_vectors<4,3>(coords, vertIndices);
+        for (int j = 0; j < 4; j++) {
+          centroids_w[dim*i+0] += vertCoords[j][0]/4;
+          centroids_w[dim*i+1] += vertCoords[j][1]/4;
+          centroids_w[dim*i+2] += vertCoords[j][2]/4;
+        } 
+      }
+      // 3D-Triangles
+      if (numFaces != 0) {
+        auto vertIndices = gather_verts<3>(elems2verts,i);
+        auto vertCoords = gather_vectors<3,3>(coords, vertIndices);
+        for (int j = 0; j < 3; j++) {
+          centroids_w[dim*i+0] += vertCoords[j][0]/3;
+          centroids_w[dim*i+1] += vertCoords[j][1]/3;
+          centroids_w[dim*i+2] += vertCoords[j][2]/3;
+        } 
+      }
+      // 3D-Edges
+      else if (numEdges != 0) {
+        auto vertIndices = gather_verts<2>(elems2verts,i);
+        auto vertCoords = gather_vectors<2,3>(coords, vertIndices);
+        for (int j = 0; j < 2; j++) {
+          centroids_w[dim*i+0] += vertCoords[j][0]/2;
+          centroids_w[dim*i+1] += vertCoords[j][1]/2;
+          centroids_w[dim*i+2] += vertCoords[j][2]/2;
+        }  
+      }
+    }
+  };
+  parallel_for(numElems,lamb,"Element Centroid Calc");
+  Read<Real> centroids(centroids_w);
+  return centroids;
+}
+
+// Finding the Midpoint of each Edge
+Read<Real> findEdgeMidpoint(Mesh& mesh,const int& dim) {
+  LO numEdges = mesh.nedges();
+  auto coords = mesh.coords();
+  auto edges2verts = mesh.ask_verts_of(EDGE);
+
+  Write<Real> midpoints_w(dim*numEdges,"Edge Midpoints");  
+  
+  auto lamb = OMEGA_H_LAMBDA(LO i) {
+    // 2D Case
+    if (dim == 2) {
+      auto vertIndices = gather_verts<2>(edges2verts,i);
+      auto vertCoords = gather_vectors<2,2>(coords, vertIndices);
+      for (int j = 0; j < 2; j++) {
+        midpoints_w[dim*i+0] += vertCoords[j][0]/2;
+        midpoints_w[dim*i+1] += vertCoords[j][1]/2;
+      }
+    }
+    // 3D Case
+    else if (dim == 3) {
+      auto vertIndices = gather_verts<2>(edges2verts,i);
+      auto vertCoords = gather_vectors<2,3>(coords, vertIndices);
+      for (int j = 0; j < 2; j++) {
+        midpoints_w[dim*i+0] += vertCoords[j][0]/2;
+        midpoints_w[dim*i+1] += vertCoords[j][1]/2;
+        midpoints_w[dim*i+2] += vertCoords[j][2]/2;
+      } 
+    }
+  }; 
+  parallel_for(numEdges,lamb,"Edge Midpoint Calc");
+  Read<Real> midpoints(midpoints_w);
+  return midpoints;
+}
+
+// Finding the centroid of each mesh using parallel for loops
+Read<Real> findElementEdgeMidpoint(Mesh& mesh, const int& dim, Read<Real>& midpointEdge) {
+  LO numElems = mesh.nelems();
+  LO numEdges = mesh.nedges();
+  LO numFaces;
+  LO order = 1;
+  if (numEdges != numElems) {
+    numFaces = mesh.nfaces();
+    order = 2;
+  }
+  LO numRegis;
+  if (numFaces != numElems) {
+    numRegis = mesh.nregions();
+    order = 3;
+  }
+  auto elems2edges = mesh.ask_down(order,1).ab2b;
+  Write<Real> avgMidpoint_w(dim*numElems,"Element Average Midpoint");  
+  
+  auto lamb = OMEGA_H_LAMBDA(LO i) {
+    // 2D Case
+    if (dim == 2) {
+      // 2D-Triangles
+      auto vertIndices = gather_verts<3>(elems2edges,i);
+      auto midpointCoords = gather_vectors<3,2>(midpointEdge, vertIndices);
+      for (int j = 0; j < 3; j++) {
+        avgMidpoint_w[dim*i+0] += midpointCoords[j][0]/3;
+        avgMidpoint_w[dim*i+1] += midpointCoords[j][1]/3;
+      }
+    }
+    // 3D Case
+    else if (dim == 3){
+      // 3D-Tets
+      if (numRegis != 0) {
+        auto vertIndices = gather_verts<6>(elems2edges,i);
+        auto midpointCoords = gather_vectors<6,3>(midpointEdge, vertIndices);
+        for (int j = 0; j < 6; j++) {
+          avgMidpoint_w[dim*i+0] += midpointCoords[j][0]/6;
+          avgMidpoint_w[dim*i+1] += midpointCoords[j][1]/6;
+          avgMidpoint_w[dim*i+2] += midpointCoords[j][2]/6;
+        }
+      }
+      // 3D-Triangles
+      if (numFaces != 0) {
+        auto vertIndices = gather_verts<3>(elems2edges,i);
+        auto midpointCoords = gather_vectors<3,2>(midpointEdge, vertIndices);
+        for (int j = 0; j < 3; j++) {
+          avgMidpoint_w[dim*i+0] += midpointCoords[j][0]/3;
+          avgMidpoint_w[dim*i+1] += midpointCoords[j][1]/3;
+          avgMidpoint_w[dim*i+2] += midpointCoords[j][2]/3;
+        }
+      }
+    }
+  };
+  parallel_for(numElems,lamb,"Element Centroid Calc");
+  Read<Real> avgMidpoint(avgMidpoint_w);
+  return avgMidpoint;
+}
+
+
 int main(int argc, char** argv) {
   auto lib = Omega_h::Library(&argc, &argv);
   if(argc!=2) {
@@ -20,73 +191,22 @@ int main(int argc, char** argv) {
   Mesh mesh(&lib);
   binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
-
-  auto elems2verts = mesh.ask_elem_verts();
-  auto vert_coords = mesh.coords();
-  vert_coords[0];
   
-  auto lamb = OMEGA_H_LAMBDA(LO i) {
-    /*if (dim == 1) {
-      auto vertIndices = gather_verts<2>(elems2verts,i);
-      auto vertCoords = gather_vectors<2,3>(coords, vertIndices);
-    }
-    else if (dim == 2) {*/
-    // auto coords = mesh.coords();
-    auto vertIndices = gather_verts<3>(elems2verts,i);
-    //auto vertCoords = gather_vectors<3,3>(coords, vertIndices);   
-      // double centroid[] = {0,0,0};
-      // vertCoords[simplex_down_template(3, 0, 0, 0)];
-     // for (int j = 0; j < 3; j++) {
-       // for (int k = 0; k < 3; k++) {
-          //centroid[k] += vertCoords[j][k];
-        //} 
-      //}   
-    /*}
-    else {
-      auto vertIndices = gather_verts<4>(elems2verts,i);
-      auto vertCoords = gather_vectors<4,3>(coords, vertIndices);
-    }*/
-  };
-  parallel_for(mesh.nelems(),lamb,"parallel_Try_1");
-  /*
-  if(!rank) {
-    fprintf(stderr, "mesh <v e f r> %d %d %d %d\n",
-        mesh.nglobal_ents(0),
-        mesh.nglobal_ents(1),
-        mesh.nglobal_ents(2),
-        dim == 3 ? mesh.nglobal_ents(3) : 0);
-  }
+  Read<Real> centroids = findCentroid(mesh,dim);
+  int order = 2;
+  if (mesh.nelems() != mesh.nfaces()) {
+    order = 3;
+  } 
 
-  auto n_edge = mesh.nedges();
-  auto edge2verts = mesh.get_adj(1,0);
-  assert(edge2verts.ab2b.size() == 2*n_edge);
-  // each edge associates with 2 vertices
+  Read<Real> midpointEdge = findEdgeMidpoint(mesh,dim);
+  mesh.add_tag<Real>(EDGE,"Edge Midpoints",dim);
+  mesh.set_tag<Real>(EDGE,"Edge Midpoints",midpointEdge);
+  
+  Read<Real> averageMidpointElem = findElementEdgeMidpoint(mesh,dim,midpointEdge);
+  mesh.add_tag<Real>(order,"Average Midpoint per Element",dim);
+  mesh.set_tag<Real>(order,"Average Midpoint per Element",averageMidpointElem);
+  
+  binary::write("./tag.osh",&mesh);
 
-  auto n_face = mesh.nfaces();
-  auto face2edges = mesh.get_adj(2,1);
-  assert(face2edges.ab2b.size() == 3*n_face);
-  // each face associates with 3 edges (all tris and tets)
-
-  if (dim == 3) {
-    auto n_cell = mesh.nelems();
-    auto cell2faces = mesh.get_adj(3,2);
-    assert(cell2faces.ab2b.size() == 4*n_cell);
-    // each cell associates with 4 faces (all tets)
-    auto face2cell = mesh.ask_up(2,3);
-    assert(face2cell.ab2b.size() == 4*n_cell);
-    // due to reverse graph from c2f
-  }
-
-  auto edge2face = mesh.ask_up(1,2);
-  assert(edge2face.ab2b.size() == 3*n_face);
-  // due to reverse graph from f2e
-
-  auto vert2edges = mesh.ask_up(0,1);
-  assert(vert2edges.ab2b.size() == 2*n_edge);
-  // due to reverse graph from e2v
-
-  auto vert2vert = mesh.ask_star(0);
-  assert(vert2vert.ab2b.size() == 2*n_edge);
-  // counts each edge twice, once from each vertex*/
   return 0;
 }
